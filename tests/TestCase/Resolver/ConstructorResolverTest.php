56c3f55a1975acd9444b92ec5a39bde6a315d814
heinrichschiller <heinrich@heinrich-schiller.de>
2025-08-21 09:05:59 +0200

    added resolveparameters return empty array when method is null test

--- tests/TestCase/Resolver/ConstructorResolverTest.php
+++ tests/TestCase/Resolver/ConstructorResolverTest.php
@@ -6,9 +6,16 @@ use PHPUnit\Framework\TestCase;
 use Selective\Container\Container;
 use Selective\Container\Exceptions\InvalidDefinitionException;
 use Selective\Container\Resolver\ConstructorResolver;
+use ReflectionMethod;
+use stdClass;
 
 final class ConstructorResolverTest extends TestCase
 {
+    /**
+     * Test.
+     * 
+     * @return void
+     */
     public function testResolveOnInvalidDefinition(): void
     {
         $this->expectException(InvalidDefinitionException::class);
@@ -18,5 +25,26 @@ final class ConstructorResolverTest extends TestCase
 
         $constructorResolver = new ConstructorResolver($container);
         $constructorResolver->resolve($invalidId);
+    }
+
+    /**
+     * Resolve parameters returns empty array when method is null.
+     * 
+     * @return void
+     */
+    public function testResolveParametersReturnsEmptyArrayWhenMethodIsNull(): void
+    {
+        $container = new Container();
+        $resolver = new ConstructorResolver($container);
+
+        $ref = new ReflectionMethod(
+            ConstructorResolver::class,
+            'resolveParameters'
+        );
+        $ref->setAccessible(true);
+
+        $result = $ref->invoke($resolver, stdClass::class, null);
+
+        $this->assertSame([], $result);
     }
 }
